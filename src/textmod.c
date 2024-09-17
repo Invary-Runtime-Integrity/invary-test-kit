758df88e021f8da888150887fad9bbee60cb55a3
Paul Couto <paul@invary.com>
2024-09-17 13:47:11 -0400

    Fixed issue with older kernels (no __sys_listen)
    Signed-off-by: Paul Couto <paul@invary.com>

--- src/textmod.c
+++ src/textmod.c
@@ -27,7 +27,7 @@
 #include "mem.h"
 #include "textmod.h"
 
-static unsigned long sys_listen = 0;
+static unsigned long sys_listen_location = 0;
 static unsigned long textmod_listen = 0;
 static unsigned char orig_data[5] = { 0 };
 static unsigned char call_data[] = { 0xe8, 0, 0, 0, 0};
@@ -41,24 +41,26 @@ void modified_listen(void)
 
 static void textmod_enable(void)
 {
-    long unsigned int cr0;
-
     // Modify sys_listen fentry
-    cr0 = unprotect_memory();
-    memcpy((void*)sys_listen, call_data, 5);
-    protect_memory(cr0);
-    printk(KERN_INFO INVARY_TEST_KIT_TAG "Modified sys_listen function entry!\n");
+    if (sys_listen_location) {
+        long unsigned int cr0;
+        cr0 = unprotect_memory();
+        memcpy((void*)sys_listen_location, call_data, 5);
+        protect_memory(cr0);
+        printk(KERN_INFO INVARY_TEST_KIT_TAG "Modified sys_listen function entry!\n");
+    }
 }
 
 static void textmod_disable(void)
 {
-    long unsigned int cr0;
-
     // Restore sys_listen
-    cr0 = unprotect_memory();
-    memcpy((void*)sys_listen, orig_data, 5);
-    protect_memory(cr0);
-    printk(KERN_INFO INVARY_TEST_KIT_TAG "Restored sys_listen function entry\n");
+    if (sys_listen_location) {
+        long unsigned int cr0;
+        cr0 = unprotect_memory();
+        memcpy((void*)sys_listen_location, orig_data, 5);
+        protect_memory(cr0);
+        printk(KERN_INFO INVARY_TEST_KIT_TAG "Restored sys_listen function entry\n");
+    }
 }
 
 
@@ -66,10 +68,15 @@ void textmod_init(void)
 {
     int offset;
 
-    sys_listen = kernel_symbol_lookup("__sys_listen");
-    if (!sys_listen) {
+    sys_listen_location = kernel_symbol_lookup("__sys_listen");
+    if (!sys_listen_location) {
         printk(KERN_WARNING INVARY_TEST_KIT_TAG "No __sys_listen!\n");
-        return;
+        // For older kernels
+        sys_listen_location = kernel_symbol_lookup("sys_listen");
+        if (!sys_listen_location) {
+            printk(KERN_WARNING INVARY_TEST_KIT_TAG "No sys_listen!\n");
+            return;
+        }
     }
 
     textmod_listen = kernel_symbol_lookup("modified_listen");
@@ -79,11 +86,11 @@ void textmod_init(void)
     }
 
     // Create function entry data
-    offset = textmod_listen - sys_listen - 5;
+    offset = textmod_listen - sys_listen_location - 5;
     memcpy(call_data+1, (void*)&offset, 4);
 
     // Save original data
-    memcpy(orig_data, (void*)sys_listen, 5);
+    memcpy(orig_data, (void*)sys_listen_location, 5);
 
     // Modify kernel text
     textmod_enable();
