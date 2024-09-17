0cd9172afd289b76e212919cb01258202aad37b0
Paul Couto <paul@invary.com>
2024-09-17 08:32:32 -0400

    Added text section modifications via fentry overwrite
    Signed-off-by: Paul Couto <paul@invary.com>

---
+++ src/textmod.c
@@ -0,0 +1,96 @@
+/*****************************************************************************
+ * Invary Test Kit Module
+ * Copyright (C) 2024 Invary, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For the complete terms of the GNU General Public License, please see this URL:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ ****************************************************************************/
+
+#include <linux/types.h>
+#include <linux/syscalls.h>
+#include <linux/printk.h>
+#include <linux/string.h>
+
+#include "symbols.h"
+#include "invary-test-kit.h"
+#include "mem.h"
+#include "textmod.h"
+
+static unsigned long sys_listen = 0;
+static unsigned long textmod_listen = 0;
+static unsigned char orig_data[5] = { 0 };
+static unsigned char call_data[] = { 0xe8, 0, 0, 0, 0};
+
+void modified_listen(void);
+
+void modified_listen(void)
+{
+    printk("Hooked sys_listen!\n");
+}
+
+static void textmod_enable(void)
+{
+    long unsigned int cr0;
+
+    // Modify sys_listen fentry
+    cr0 = unprotect_memory();
+    memcpy((void*)sys_listen, call_data, 5);
+    protect_memory(cr0);
+    printk(KERN_INFO INVARY_TEST_KIT_TAG "Modified sys_listen function entry!\n");
+}
+
+static void textmod_disable(void)
+{
+    long unsigned int cr0;
+
+    // Restore sys_listen
+    cr0 = unprotect_memory();
+    memcpy((void*)sys_listen, orig_data, 5);
+    protect_memory(cr0);
+    printk(KERN_INFO INVARY_TEST_KIT_TAG "Restored sys_listen function entry\n");
+}
+
+
+void textmod_init(void)
+{
+    int offset;
+
+    sys_listen = kernel_symbol_lookup("__sys_listen");
+    if (!sys_listen) {
+        printk(KERN_WARNING INVARY_TEST_KIT_TAG "No __sys_listen!\n");
+        return;
+    }
+
+    textmod_listen = kernel_symbol_lookup("modified_listen");
+    if (!textmod_listen) {
+        printk(KERN_WARNING INVARY_TEST_KIT_TAG "No textmod_listen!\n");
+        return;
+    }
+
+    // Create function entry data
+    offset = textmod_listen - sys_listen - 5;
+    memcpy(call_data+1, (void*)&offset, 4);
+
+    // Save original data
+    memcpy(orig_data, (void*)sys_listen, 5);
+
+    // Modify kernel text
+    textmod_enable();
+}
+
+void textmod_shutdown(void)
+{
+    // Restore modifications
+    textmod_disable();
+}
